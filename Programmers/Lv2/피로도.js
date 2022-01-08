29b6f54385aa5b1628543ed75707d53f33f1f293
JunHyeongB <aowlr0513@gmail.com>
2022-01-08 20:50:17 +0900

    add: 피로도 | 22-01-08

---
+++ Programmers/Lv2/피로도.js
@@ -0,0 +1,28 @@
+function solution(k, dungeons) {
+  const n = dungeons.length;
+  const check = Array(n).fill(0);
+  let answer = 0;
+
+  const DFS = (L, rest, count) => {
+    if (answer === n) return;
+    if (L === n) {
+      answer = Math.max(answer, count);
+    } else {
+      for (let i = 0; i < n; i++) {
+        if (check[i] === 0) {
+          check[i] = 1;
+          if (dungeons[i][0] <= rest && dungeons[i][1] <= rest) {
+            DFS(L + 1, rest - dungeons[i][1], count + 1);
+          } else {
+            DFS(L + 1, rest, count);
+          }
+          check[i] = 0;
+        }
+      }
+    }
+  };
+
+  DFS(0, k, 0);
+
+  return answer;
+}

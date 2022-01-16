cd34b5299eae4fd6cf8414a1945b1f0cc4371264
JunHyeongB <aowlr0513@gmail.com>
2022-01-16 20:56:31 +0900

    add: 괄호 회전하기 | 22-01-16

---
+++ Programmers/Lv2/괄호 회전하기.js
@@ -0,0 +1,49 @@
+const isProperBracket = (s) => {
+  const stack = [];
+
+  for (const bracket of s) {
+    const lastBracket = stack[stack.length - 1];
+    switch (bracket) {
+      case "(":
+      case "[":
+      case "{":
+        stack.push(bracket);
+        break;
+      case ")":
+        if (!lastBracket) return false;
+        if (lastBracket === "(") {
+          stack.pop();
+        }
+        break;
+      case "]":
+        if (!lastBracket) return false;
+        if (lastBracket === "[") {
+          stack.pop();
+        }
+        break;
+      case "}":
+        if (!lastBracket) return false;
+        if (lastBracket === "{") {
+          stack.pop();
+        }
+        break;
+    }
+  }
+
+  return !stack.length;
+};
+
+function solution(s) {
+  const tempArray = s.split("");
+  let count = 0;
+
+  for (const i in tempArray) {
+    if (isProperBracket(tempArray)) {
+      count += 1;
+    }
+
+    tempArray.push(tempArray.shift());
+  }
+
+  return count;
+}

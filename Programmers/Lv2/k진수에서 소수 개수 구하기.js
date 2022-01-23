adca4f599437a1c258b2bc6e01713f7aa5a8af02
JunHyeongB <aowlr0513@gmail.com>
2022-01-23 18:13:16 +0900

    add: k진수에서 소수 개수 구하기 | 22-01-23

---
+++ Programmers/Lv2/k진수에서 소수 개수 구하기.js
@@ -0,0 +1,25 @@
+const isPrime = (num) => {
+  if (num % 2 === 0) {
+    return num === 2;
+  }
+
+  for (let i = 3; i <= Math.ceil(num ** 0.5); i += 2) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+};
+
+function solution(n, k) {
+  const conversedNumber = n.toString(k).split("0");
+  let answer = 0;
+
+  conversedNumber.forEach((num) => {
+    if (isPrime(+num)) {
+      answer++;
+    }
+  });
+
+  return answer;
+}

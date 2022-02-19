81ab9d8585fa736c5286d30b1b7b440614cfac78
JunHyeongB <aowlr0513@gmail.com>
2022-02-19 19:48:12 +0900

    add: Merge Sorted Array / Subsets II | 22-02-19

---
+++ LeetCode/Medium/90. Subsets II.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function (nums) {
+  const n = nums.length;
+  const answer = [];
+  const map = new Map();
+
+  const DFS = (L, tmp) => {
+    if (L === n) {
+      const newArray = [...tmp].sort((a, b) => a - b);
+      map.set(newArray.join(""), newArray);
+    } else {
+      DFS(L + 1, [...tmp, nums[L]]);
+      DFS(L + 1, [...tmp]);
+    }
+  };
+
+  DFS(0, []);
+
+  for (const [_, value] of map) {
+    answer.push([...value]);
+  }
+
+  return answer;
+};

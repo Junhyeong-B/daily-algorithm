dbe0c2d5ac7222dafa7f3360e44d23531cee50f2
JunHyeongB <aowlr0513@gmail.com>
2022-02-27 20:03:35 +0900

    add: Excel Sheet Column Title, Number / Majority Element | 22-02-27

---
+++ LeetCode/Easy/168. Excel Sheet Column Title.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function (columnNumber) {
+  let answer = "";
+  let num = columnNumber;
+
+  while (num) {
+    if (num % 26 === 0) {
+      answer = "Z" + answer;
+      num = Math.floor(num / 26) - 1;
+    } else {
+      answer = String.fromCharCode((num % 26) + 64) + answer;
+      num = Math.floor(num / 26);
+    }
+  }
+
+  return answer;
+};

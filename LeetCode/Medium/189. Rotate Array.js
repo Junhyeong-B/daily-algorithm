d768683f2f4914b131cb23f1f17418191251e93a
JunHyeongB <aowlr0513@gmail.com>
2022-03-03 15:05:19 +0900

    add: Best Time to Buy and Sell Stock IV / Rotate Array | 22-03-03

---
+++ LeetCode/Medium/189. Rotate Array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  const copiedArray = [...nums];
+  let startIndex = k % nums.length;
+
+  for (let i = 0; i < nums.length; i++) {
+    nums[startIndex] = copiedArray[i];
+    if (startIndex === nums.length - 1) {
+      startIndex = 0;
+    } else {
+      startIndex++;
+    }
+  }
+};

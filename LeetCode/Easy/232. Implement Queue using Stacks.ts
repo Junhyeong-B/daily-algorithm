9f376a50dee4be892f0888a68abbd7ca932463a7
JunHyeongB <aowlr0513@gmail.com>
2022-03-13 14:34:54 +0900

    add: 행렬 테두리 회전하기 / Implement Queue using Stacks | 22-03-13

---
+++ LeetCode/Easy/232. Implement Queue using Stacks.ts
@@ -0,0 +1,48 @@
+{
+  /*
+    Runtime: 79 ms, faster than 72.73% of TypeScript online submissions for Implement Queue using Stacks.
+    Memory Usage: 43.4 MB, less than 14.77% of TypeScript online submissions for Implement Queue using Stacks.
+  */
+  class MyQueue {
+    queue: Array<number | undefined>;
+    head: number;
+    tail: number;
+    size: number;
+    constructor() {
+      this.queue = [];
+      this.head = 0;
+      this.tail = 0;
+      this.size = 0;
+    }
+
+    push(x: number): void {
+      this.queue[this.tail] = x;
+      this.tail += 1;
+      this.size += 1;
+    }
+
+    pop(): number {
+      const value = this.queue[this.head];
+      this.queue[this.head] = undefined;
+      this.head += 1;
+      this.size -= 1;
+      return value!;
+    }
+
+    peek(): number {
+      return this.queue[this.head]!;
+    }
+
+    empty(): boolean {
+      return this.size === 0;
+    }
+  }
+
+/**
+* Your MyQueue object will be instantiated and called as such:
+* var obj = new MyQueue()
+* obj.push(x)
+* var param_2 = obj.pop()
+* var param_3 = obj.peek()
+* var param_4 = obj.empty()
+*/}

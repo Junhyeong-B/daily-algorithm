7a65096925eb03ce9062c34d3bdf2d4d9ccf956f
JunHyeongB <aowlr0513@gmail.com>
2022-04-15 15:27:13 +0900

    add: Trim a Binary Search Tree / 부족한 금액 계산하기, 나머지가 1이 되는 수 | 22-04-15

---
+++ LeetCode/Medium/669. Trim a Binary Search Tree.ts
@@ -0,0 +1,37 @@
+{
+  /**
+   * Runtime: 78 ms, faster than 87.10% of TypeScript online submissions for Trim a Binary Search Tree.
+   * Memory Usage: 49 MB, less than 74.19% of TypeScript online submissions for Trim a Binary Search Tree.
+   */
+
+  /**
+   * Definition for a binary tree node.
+   */
+  class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+      this.val = (val === undefined ? 0 : val)
+      this.left = (left === undefined ? null : left)
+      this.right = (right === undefined ? null : right)
+    }
+  }
+
+  function trimBST(root: TreeNode | null, low: number, high: number): TreeNode | null {
+    if (root === null) {
+      return null;
+    }
+
+    if (root.val > high) {
+      return trimBST(root.left, low, high);
+    } else if (root.val < low) {
+      return trimBST(root.right, low, high);
+    }
+
+    root.left = trimBST(root.left, low, high);
+    root.right = trimBST(root.right, low, high);
+
+    return root;
+  };
+}

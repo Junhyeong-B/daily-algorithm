fc2137332c79ccd8c1f82ffd4f2bd1f6404dc819
JunHyeongB <aowlr0513@gmail.com>
2022-06-16 20:46:58 +0900

    add: Longest Palindromic Substring | 22-06-16

---
+++ LeetCode/Medium/5. Longest Palindromic Substring(220616).ts
@@ -0,0 +1,35 @@
+{
+  /**
+   * Runtime: 138 ms, faster than 79.80% of TypeScript online submissions for Longest Palindromic Substring.
+   * Memory Usage: 46.1 MB, less than 68.37% of TypeScript online submissions for Longest Palindromic Substring.
+   */
+
+  function longestPalindrome(s: string): string {
+    let longestPalindrome = "";
+
+    for (let i = 0; i < s.length; i++) {
+      let left = i;
+      let right = i;
+
+      while (left >= 0 && right <= s.length - 1 && s[left] === s[right]) {
+        if (longestPalindrome.length < right - left + 1) {
+          longestPalindrome = s.slice(left, right + 1);
+        }
+        left--;
+        right++;
+      }
+
+      left = i;
+      right = i + 1;
+      while (left >= 0 && right <= s.length - 1 && s[left] === s[right]) {
+        if (longestPalindrome.length < right - left + 1) {
+          longestPalindrome = s.slice(left, right + 1);
+        }
+        left--;
+        right++;
+      }
+    }
+
+    return longestPalindrome;
+  };
+}

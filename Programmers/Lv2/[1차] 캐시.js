f7f22297f6bfe08a299f82c88b673c93ffcdce2b
JunHyeongB <aowlr0513@gmail.com>
2022-09-01 00:02:08 +0900

    add: [1차] 캐시

--- Programmers/Lv2/[1차] 캐시.js
+++ Programmers/Lv2/[1차] 캐시.js
@@ -5,13 +5,13 @@ function solution(cacheSize, cities) {
 
   let execTime = 0;
   const citiesWithLowerCase = cities.map((city) => city.toLowerCase());
-  const queue = [1, 2, 3];
+  const queue = [];
 
   for (const city of citiesWithLowerCase) {
     const index = queue.indexOf(city);
 
     if (index === -1) {
-      if (queue.length > cacheSize) {
+      if (queue.length >= cacheSize) {
         queue.shift();
       }
       execTime += 5;
@@ -26,19 +26,29 @@ function solution(cacheSize, cities) {
   return execTime;
 }
 
-console.log(
-  solution(5, [
-    "Jeju",
-    "Pangyo",
-    "Seoul",
-    "NewYork",
-    "LA",
-    "SanFrancisco",
-    "Seoul",
-    "Rome",
-    "Paris",
-    "Jeju",
-    "NewYork",
-    "Rome",
-  ])
-);
+/**
+ * 정확성  테스트
+ *  테스트 1 〉	통과 (0.15ms, 30.1MB)
+ *  테스트 2 〉	통과 (0.11ms, 30MB)
+ *  테스트 3 〉	통과 (0.14ms, 30.1MB)
+ *  테스트 4 〉	통과 (0.12ms, 30.1MB)
+ *  테스트 5 〉	통과 (0.15ms, 30MB)
+ *  테스트 6 〉	통과 (0.07ms, 29.7MB)
+ *  테스트 7 〉	통과 (0.12ms, 29.8MB)
+ *  테스트 8 〉	통과 (0.12ms, 30MB)
+ *  테스트 9 〉	통과 (0.09ms, 30MB)
+ *  테스트 10 〉통과 (0.11ms, 29.9MB)
+ *  테스트 11 〉통과 (44.39ms, 41.9MB)
+ *  테스트 12 〉통과 (0.12ms, 30.2MB)
+ *  테스트 13 〉통과 (0.20ms, 29.9MB)
+ *  테스트 14 〉통과 (0.18ms, 30.1MB)
+ *  테스트 15 〉통과 (0.18ms, 30.1MB)
+ *  테스트 16 〉통과 (0.20ms, 30.1MB)
+ *  테스트 17 〉통과 (0.06ms, 30MB)
+ *  테스트 18 〉통과 (0.32ms, 30.2MB)
+ *  테스트 19 〉통과 (0.66ms, 29.8MB)
+ *  테스트 20 〉통과 (0.40ms, 30.1MB)
+ * 채점 결과
+ *  정확성: 100.0
+ *  합계: 100.0 / 100.0
+ */
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 # 🚩 Javascript 코딩테스트 Repository
 - 코딩테스트 연습 저장소입니다.
 ---
-# 현재까지 푼 문제 수: 🐥 255개
+# 현재까지 푼 문제 수: 🐥 257개
 ## BOJ
 ### Bronze
 - [2798 블랙잭](./BOJ/Bronze/2798%20%EB%B8%94%EB%9E%99%EC%9E%AD.js)
@@ -245,6 +245,8 @@
 - [전력망을 둘로 나누기](./Programmers/Lv2/%EC%A0%84%EB%A0%A5%EB%A7%9D%EC%9D%84%20%EB%91%98%EB%A1%9C%20%EB%82%98%EB%88%84%EA%B8%B0.js)
 - [괄호 회전하기](./Programmers/Lv2/%EA%B4%84%ED%98%B8%20%ED%9A%8C%EC%A0%84%ED%95%98%EA%B8%B0.js)
 - [문자열 압축](./Programmers/Lv2/%EB%AC%B8%EC%9E%90%EC%97%B4%20%EC%95%95%EC%B6%95.js)
+- [위장](./Programmers/Lv2/%EC%9C%84%EC%9E%A5.js)
+- [H-index](./Programmers/Lv2/H-index.js)
 - [후보키](./Programmers/Lv2/%ED%9B%84%EB%B3%B4%ED%82%A4.js)
 - [프린터](./Programmers/Lv2/%ED%94%84%EB%A6%B0%ED%84%B0.js)
 - [모음 사전](./Programmers/Lv2/%EB%AA%A8%EC%9D%8C%20%EC%82%AC%EC%A0%84.js)

9efbba363800fa892da414838191fb7d07d248cc
Junhyeong-B <aowlr0513@gmail.com>
2022-11-10 23:56:02 +0900

    add: 1047. Remove All Adjacent Duplicates In String

---
+++ LeetCode/Easy/1047. Remove All Adjacent Duplicates In String.ts
@@ -0,0 +1,24 @@
+/**
+ * Runtime 148 ms Beats 62.7%
+ * Memory 52.7 MB Beats 62.7%
+ */
+
+function removeDuplicates(s: string): string {
+  const stack: string[] = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (stack.length === 0) {
+      stack.push(s[i]);
+    } else {
+      if (stack[stack.length - 1] === s[i]) {
+        stack.pop();
+      } else {
+        stack.push(s[i]);
+      }
+    }
+  }
+
+  return stack.join('');
+}
+
+console.log(removeDuplicates('abbaca')); // ca
